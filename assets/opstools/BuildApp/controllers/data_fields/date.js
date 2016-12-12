7b8847d892ffd8380e078a329eb2d00fe9a92503
kidlovess <stdsitsuwan2008@gmail.com>
2016-12-12 21:56:22 +0700

    Update date.js

--- assets/opstools/BuildApp/controllers/data_fields/date.js
+++ assets/opstools/BuildApp/controllers/data_fields/date.js
@@ -32,6 +32,71 @@ steal(function () {
 				labelRight: "Include time",
 				labelWidth: 0
 			},
+			{
+				view: "label",
+				label: "Date format options"
+			},
+			{
+				view: "text",
+				label: "Date Display",
+				labelWidth: "100",
+				id: "date-display",
+				placeholder: "date-display"
+			},
+			{
+		
+				view: "richselect",
+				id: componentIds.includeDay,
+				label: "Day",
+				value: 'none',
+				options: [
+					{ id: 'includeDay-d', value: "1" },
+					{ id: 'includeDay-dd', value: "01" },
+					{ id: 'includeDay-ddd', value: "Fri" },
+					{ id: 'includeDay-dddd', value: "Monday" }
+					]
+	
+			},
+			{
+		
+				view: "richselect",
+				id: componentIds.includeMonth,
+				label: "Month",
+				value: 'none',
+				options: [
+					{ id: 'includeDay-M', value: "1" },
+					{ id: 'includeDay-MM', value: "01" },
+					{ id: 'includeDay-MMM', value: "Jun" },
+					{ id: 'includeDay-MMMM', value: "June" }
+					]
+	
+			},
+			{
+		
+				view: "richselect",
+				id: componentIds.includeYear,
+				label: "Year",
+				value: 'none',
+				options: [
+					{ id: 'includeDay-Y', value: "1" },
+					{ id: 'includeDay-YY', value: "01" },
+					{ id: 'includeDay-YYY', value: "001" },
+					{ id: 'includeDay-YYYY', value: "0001" }
+					]
+	
+			},
+			{
+				view: "radio",
+				id: componentIds.typeDate,
+				label: "TypeDate",
+				value: 'none',
+				vertical: true,
+				options: [
+					{ id: 'comma',  "Comma" },
+					{ id: 'period', "Period" },
+					{ id: 'space',  "Space" }
+				]
+			}
 		]
 	};
 
